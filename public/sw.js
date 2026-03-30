f14afabb6bbf70901f2b27a7e34b941393870426
Jaosuzart <joaomarcelo@gmail.com>
2026-03-30 18:57:18 -0300

    Enviando env ao github

--- public/sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Instalado');
-});
-
-self.addEventListener('fetch', (e) => {
-    // Apenas repassa as requisições (PWA básico)
-});
