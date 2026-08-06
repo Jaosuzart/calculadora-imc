6419bba59b18c0f91fb43c2101c27e3815cc4e0b
Jaosuzart <joaomarcelosuzartcastro@gmail.com>
2026-08-06 19:19:20 -0300

    Enviando atualizacao ao github

--- src/main.js
+++ src/main.js
@@ -107,12 +107,29 @@ if (btnGoogle) {
     if (error) {
       alert("Erro ao logar com Google: " + error.message);
       btnGoogle.textContent = "";
-      const imgIcon = document.createElement("img");  
-      imgIcon.src = "/google-logo.png";
-      imgIcon.width = 20;
-      imgIcon.className = "me-2 bg-white rounded-circle p-1";
-      btnGoogle.append(imgIcon, " Entrar com Google");
-      btnGoogle.disabled = false;
+      const svgIcon = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+      svgIcon.setAttribute("viewBox", "0 0 48 48");
+      svgIcon.setAttribute("width", "20");
+      svgIcon.setAttribute("height", "20");
+      svgIcon.setAttribute("class", "me-2 bg-white rounded-circle p-1");
+
+      const pathVermelho = document.createElementNS("http://www.w3.org/2000/svg", "path");
+      pathVermelho.setAttribute("fill", "#EA4335");
+      pathVermelho.setAttribute("d", "M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.7 17.74 9.5 24 9.5z");
+
+      const pathAzul = document.createElementNS("http://www.w3.org/2000/svg", "path");
+      pathAzul.setAttribute("fill", "#4285F4");
+      pathAzul.setAttribute("d", "M46.98 24.55c0-1.57-.15-3.09-.38-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6c4.51-4.18 7.09-10.36 7.09-17.65z");
+
+      const pathAmarelo = document.createElementNS("http://www.w3.org/2000/svg", "path");
+      pathAmarelo.setAttribute("fill", "#FBBC05");
+      pathAmarelo.setAttribute("d", "M10.53 28.59c-.48-1.45-.76-2.99-.76-4.59s.27-3.14.76-4.59l-7.98-6.19C.92 16.46 0 20.12 0 24c0 3.88.92 7.54 2.56 10.78l7.97-6.19z");
+
+      const pathVerde = document.createElementNS("http://www.w3.org/2000/svg", "path");
+      pathVerde.setAttribute("fill", "#34A853");
+      pathVerde.setAttribute("d", "M24 48c6.48 0 11.93-2.13 15.89-5.81l-7.73-6c-2.15 1.45-4.92 2.3-8.16 2.3-6.26 0-11.57-4.22-13.47-9.91l-7.98 6.19C6.51 42.62 14.62 48 24 48z");
+
+      svgIcon.append(pathVermelho, pathAzul, pathAmarelo, pathVerde);    
     }
   });
 }
